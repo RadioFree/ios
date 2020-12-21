bec19d97d27b60ad1d8680e25a14d4a0ffeb7b5f
Chase Lang <chase@chaselang.com>
2020-12-21 11:57:51 -0700

    Add files via upload

---
+++ OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by vivek on 18/05/18.
+//  Copyright © 2018 Aimperior. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
